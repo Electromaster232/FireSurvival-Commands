85f10a478d22feb2bcc147c1ee954d18799f7bc8
Electromaster232 <gilbert7573@gmail.com>
2017-07-25 12:52:22 -0400

    Push request

--- src/com/theendlessweb/electromaster/Main.java
+++ src/com/theendlessweb/electromaster/Main.java
@@ -1,18 +1,34 @@
 package com.theendlessweb.electromaster;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
+
+/* 
+ * Created by xxq and Electromaster on 7/25/17.
+ */
 
 public class Main extends JavaPlugin {
 	
 	 	private Connection connection;
 	    private String host, database, username, password;
 	    private int port;
-}
+	    Map<UUID, Long> users;
+
 	@Override
 	public void onEnable() {
+		users = new HashMap<UUID, Long>();
 		host = "localhost";
 		port = 3306;
 		database = "TestDatabase";
@@ -21,12 +37,17 @@ public class Main extends JavaPlugin {
 		try {     
 			openConnection();
 			Statement statement = connection.createStatement(); 
-			log.info(“Plugin enabled.”);   
+			getLogger().info("Plugin enabled.");  
+		}catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}catch (SQLException e){
+			e.printStackTrace();
 		}
+	}
 
 		@Override
 		public void onDisable() {
-			log.info(“Plugin disabled.”);
+			getLogger().info("Plugin disabled.");
 		}
 
 		@Override
@@ -38,7 +59,30 @@ public class Main extends JavaPlugin {
 				sender.sendMessage("You ran /mycommand!");
 				return true;
 			}
+			else if(command.getName().equalsIgnoreCase("eat")){
+				if(sender instanceof Player && args.length == 0){
+					Player player = (Player) sender;
+					player.setSaturation(20.0F);
+					player.setFoodLevel(20);
+					
+				}else{
+					sender.sendMessage("Invalid syntax for this command, use /eat.");
+				}
+			//next command here.
+			}
 			return false;
 		}
-
+	
+	/* Events */
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event){
+		Player player = event.getPlayer();
+		// Do something here later.
 	}
+	
+	 public void openConnection() throws SQLException, ClassNotFoundException {
+		    if (connection != null && !connection.isClosed()) {
+		        return;
+		    }
+	 }	
+}	 
