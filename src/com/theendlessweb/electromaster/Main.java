f10ecd9d1ca700f09dcc8bb7e030fe288a13dfe3
Electromaster232 <gilbert7573@gmail.com>
2017-07-25 13:18:27 -0400

    Push request

--- src/com/theendlessweb/electromaster/Main.java
+++ src/com/theendlessweb/electromaster/Main.java
@@ -7,11 +7,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -64,11 +68,56 @@ public class Main extends JavaPlugin {
 					Player player = (Player) sender;
 					player.setSaturation(20.0F);
 					player.setFoodLevel(20);
+					return true;
 					
 				}else{
 					sender.sendMessage("Invalid syntax for this command, use /eat.");
+					return false;
 				}
+			}
+			
 			//next command here.
+			else if(command.getName().equalsIgnoreCase("jihad")){
+				if(sender instanceof Player && args.length == 0){
+					if ((users.get(((Player) sender).getUniqueId()) == null)) {
+						ItemStack tnt = new ItemStack(Material.TNT, 64);
+	                    ItemStack lighter = new ItemStack(Material.FLINT_AND_STEEL, 1);
+	                    ItemMeta lightermeta = lighter.getItemMeta();
+	                    ItemMeta TNTMeta = tnt.getItemMeta();
+	                    lightermeta.setDisplayName("xxq's Flint/Steel"); 
+	                    lightermeta.addEnchant(Enchantment.DURABILITY, 32767, false);
+	                    TNTMeta.setDisplayName("xxq's Dynamite");
+	                    TNTMeta.addEnchant(Enchantment.MENDING, 5, false);
+	                    TNTMeta.addEnchant(Enchantment.DURABILITY, 32767, false);;
+	                    tnt.setItemMeta(TNTMeta);
+	                    lighter.setItemMeta(lightermeta);
+	                    ((Player) sender).getInventory().addItem(tnt, lighter);
+	                    return true;
+					}
+				}else{
+					sender.sendMessage("Invalid syntax for this command. Use /jihad.");
+					return false;
+				}
+			
+			}
+			else if(command.getName().equalsIgnoreCase("goditems")){
+				if(sender instanceof Player && args.length == 0){
+					if((users.get(((Player) sender).getUniqueId()) == null)){
+						ItemStack sword = new ItemStack(Material.DIAMOND_SWORD, 64);
+						ItemMeta swordMeta = sword.getItemMeta();
+						swordMeta.setDisplayName("xxq's Abyssal Blade");
+						swordMeta.addEnchant(Enchantment.DAMAGE_ALL, 32767, false); // Sharpness
+						swordMeta.addEnchant(Enchantment.DURABILITY, 32767, false); // Unbreaking
+						swordMeta.addEnchant(Enchantment.FIRE_ASPECT, 32767, false); // Fire Aspect
+						swordMeta.addEnchant(Enchantment.MENDING, 32767, false); // Mending
+						swordMeta.addEnchant(Enchantment.LOOT_BONUS_BLOCKS, 10, false); // Looting for blocks
+						swordMeta.addEnchant(Enchantment.SWEEPING_EDGE, 32767, false); 
+						swordMeta.addEnchant(Enchantment.VANISHING_CURSE, 1, false); // Vanishing curse
+						sword.setItemMeta(swordMeta);
+						((Player) sender).getInventory().addItem(sword);
+						
+					}
+				}
 			}
 			return false;
 		}
@@ -84,9 +133,7 @@ public class Main extends JavaPlugin {
 		    if (connection != null && !connection.isClosed()) {
 		        return;
 		    }
-	 }	
-<<<<<<< HEAD
-}	 
-=======
-}	 
->>>>>>> 9d1d53fc0ce3dbf8153bfd7e405ba9b85a6b1f7c
+	 
+	 
+	 }
+}
