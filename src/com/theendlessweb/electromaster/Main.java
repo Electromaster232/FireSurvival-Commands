eeb27d163dd42c0cedeff6ba40d42c9885848a92
Electromaster232 <gilbert7573@gmail.com>
2017-07-26 10:45:41 -0400

    Added Axe command /weapon

--- src/com/theendlessweb/electromaster/Main.java
+++ src/com/theendlessweb/electromaster/Main.java
@@ -27,12 +27,12 @@ import net.md_5.bungee.api.ChatColor;
  */
 
 public class Main extends JavaPlugin {
-	
-	 	private Connection connection;
-	    private String host, database, username, password;
-	    private int port;
-	    private Map<UUID, Long> users;
-	    public static final String PLUGIN_VERSION = "1.2";
+
+	private Connection connection;
+	private String host, database, username, password;
+	private int port;
+	private Map<UUID, Long> users;
+	public static final String PLUGIN_VERSION = "1.2";
 
 	@Override
 	public void onEnable() {
@@ -53,111 +53,136 @@ public class Main extends JavaPlugin {
 		}
 	}
 
-		@Override
-		public void onDisable() {
-			getLogger().info("Plugin disabled.");
-		}
-
-		@Override
-		public boolean onCommand(CommandSender sender,
-				Command command,
-				String label,
-				String[] args) {
-			if (command.getName().equalsIgnoreCase("worldnews")) {
-				sender.sendMessage("§eYou ran /mycommand!");
-				return true;
-			}
-			else if(command.getName().equalsIgnoreCase("eat")){
-				if(sender instanceof Player && args.length == 0){
-					Player player = (Player) sender;
-					player.setSaturation(40.0F);
-					player.setFoodLevel(20);
-					player.setHealth(20.0F);
-					sender.sendMessage(ccMsg("Your player has been restored."));
-					return true;
-					
-				}else{
-					sender.sendMessage(ccErrMsg("Invalid syntax!"));
-					return false;
-				}
-			}
-			
-			//next command here.
-			else if(command.getName().equalsIgnoreCase("supertnt")){
-				if(sender instanceof Player && args.length == 0){
-					if ((users.get(((Player) sender).getUniqueId()) == null)) {
-						ItemStack tnt = new ItemStack(Material.TNT, 64);
-	                    ItemStack lighter = new ItemStack(Material.FLINT_AND_STEEL, 1);
-	                    ItemMeta lightermeta = lighter.getItemMeta();
-	                    ItemMeta TNTMeta = tnt.getItemMeta();
-	                    lightermeta.setDisplayName("xxq's Flint/Steel"); 
-	                    lightermeta.addEnchant(Enchantment.DURABILITY, 3, false);
-	                    TNTMeta.setDisplayName("xxq's Dynamite");
-	                    TNTMeta.addEnchant(Enchantment.MENDING, 1, false);
-	                    TNTMeta.addEnchant(Enchantment.DURABILITY, 3, false);;
-	                    tnt.setItemMeta(TNTMeta);
-	                    lighter.setItemMeta(lightermeta);
-	                    ((Player) sender).getInventory().addItem(tnt, lighter);
-	                    sender.sendMessage(ccMsg("Enjoy your TNT!"));
-	                    return true;
-					}
-				}else{
-					sender.sendMessage(ccErrMsg("Invalid syntax!"));
-					return false;
-				}
-			
-			}
-			else if(command.getName().equalsIgnoreCase("sword")){
-				if(sender instanceof Player && args.length == 0){
-					if((users.get(((Player) sender).getUniqueId()) == null)){
-						ItemStack sword = new ItemStack(Material.DIAMOND_SWORD, 1);
-						ItemMeta swordMeta = sword.getItemMeta();
-						swordMeta.setDisplayName("xxq's Abyssal Blade");
-						swordMeta.setUnbreakable(true);
-						swordMeta.addEnchant(Enchantment.DAMAGE_ALL, 32767, true); // Sharpness
-						swordMeta.addEnchant(Enchantment.DURABILITY, 32767, true); // Unbreaking
-						swordMeta.addEnchant(Enchantment.FIRE_ASPECT, 32767, true); // Fire Aspect
-						swordMeta.addEnchant(Enchantment.MENDING, 32767, true); // Mending
-						swordMeta.addEnchant(Enchantment.SWEEPING_EDGE, 32767, true); // Cleaving affect
-						sword.setItemMeta(swordMeta);
-						((Player) sender).getInventory().addItem(sword);
-						sender.sendMessage(ccMsg("Added xxq's Abyssal Blade to your inventory."));
-						
-					}
-				}else{
-					sender.sendMessage(ccErrMsg("Invalid syntax!"));
-
-				}
-			}
-			return false;
-		}
-	
-	/* Events */
-	@EventHandler
-	public void onJoin(PlayerJoinEvent event){
-		Player player = event.getPlayer();
-<<<<<<< HEAD
-		player.sendMessage("[CustomCommands] This server is running CustomCommands V1.0. By: Electromaster and xxq.");
-=======
-		player.sendMessage(ccMsg("This server is running CustomCommands v" + PLUGIN_VERSION + "."));
->>>>>>> da0a69ce26982e5522ca57c9b5c01adffdeb7876
+	@Override
+	public void onDisable() {
+		getLogger().info("Plugin disabled.");
 	}
-	
-	 public void openConnection() throws SQLException, ClassNotFoundException {
-		    if (connection != null && !connection.isClosed()) {
-		        return;
-		    }
 
-	 }	
-	 
-	 public String ccErrMsg(String message){
-	 	return ChatColor.BLACK + "[" + ChatColor.AQUA + "CustomCommands" + ChatColor.BLACK + "]" + " " + ChatColor.RED + message;
-	 }
-	 
-	 public String ccMsg(String message){
-		 return ChatColor.BLACK + "[" + ChatColor.AQUA + "CustomCommands" + ChatColor.BLACK + "]" + " " + ChatColor.YELLOW + message;
-	 }
-	 
+	@Override
+	public boolean onCommand(CommandSender sender,
+			Command command,
+			String label,
+			String[] args) {
+		if (command.getName().equalsIgnoreCase("worldnews")) {
+			sender.sendMessage("§eYou ran /mycommand!");
+			return true;
+		}
+		else if(command.getName().equalsIgnoreCase("eat")){
+			if(sender instanceof Player && args.length == 0){
+				Player player = (Player) sender;
+				player.setSaturation(40.0F);
+				player.setFoodLevel(20);
+				player.setHealth(20.0F);
+				sender.sendMessage(ccMsg("Your player has been restored."));
+				return true;
 
-}
- 
+			}else{
+				sender.sendMessage(ccErrMsg("Invalid syntax!"));
+				return false;
+			}
+		}
+
+		//next command here.
+		else if(command.getName().equalsIgnoreCase("supertnt")){
+			if(sender instanceof Player && args.length == 0){
+				if ((users.get(((Player) sender).getUniqueId()) == null)) {
+					ItemStack tnt = new ItemStack(Material.TNT, 64);
+					ItemStack lighter = new ItemStack(Material.FLINT_AND_STEEL, 1);
+					ItemMeta lightermeta = lighter.getItemMeta();
+					ItemMeta TNTMeta = tnt.getItemMeta();
+					lightermeta.setDisplayName("xxq's Flint/Steel"); 
+					lightermeta.addEnchant(Enchantment.DURABILITY, 3, false);
+					TNTMeta.setDisplayName("xxq's Dynamite");
+					TNTMeta.addEnchant(Enchantment.MENDING, 1, false);
+					TNTMeta.addEnchant(Enchantment.DURABILITY, 3, false);;
+					tnt.setItemMeta(TNTMeta);
+					lighter.setItemMeta(lightermeta);
+					((Player) sender).getInventory().addItem(tnt, lighter);
+					sender.sendMessage(ccMsg("Enjoy your TNT!"));
+					return true;
+				}
+			}else{
+				sender.sendMessage(ccErrMsg("Invalid syntax!"));
+				return false;
+			}
+
+		}
+		else if(command.getName().equalsIgnoreCase("sword")){
+			if(sender instanceof Player && args.length == 0){
+				if((users.get(((Player) sender).getUniqueId()) == null)){
+					ItemStack sword = new ItemStack(Material.DIAMOND_SWORD, 1);
+					ItemMeta swordMeta = sword.getItemMeta();
+					swordMeta.setDisplayName("xxq's Abyssal Blade");
+					swordMeta.setUnbreakable(true);
+					swordMeta.addEnchant(Enchantment.DAMAGE_ALL, 32767, true); // Sharpness
+					swordMeta.addEnchant(Enchantment.DURABILITY, 32767, true); // Unbreaking
+					swordMeta.addEnchant(Enchantment.FIRE_ASPECT, 32767, true); // Fire Aspect
+					swordMeta.addEnchant(Enchantment.MENDING, 32767, true); // Mending
+					swordMeta.addEnchant(Enchantment.SWEEPING_EDGE, 32767, true); // Cleaving affect
+
+					sword.setItemMeta(swordMeta);
+					((Player) sender).getInventory().addItem(sword);
+					sender.sendMessage(ccMsg("Added xxq's Abyssal Blade to your inventory."));
+
+				
+			}
+			}
+		}
+		else if(command.getName().equalsIgnoreCase("weapon")){
+			if(sender instanceof Player && args.length == 0){
+				if((users.get(((Player) sender).getUniqueId()) == null)){
+					ItemStack axe = new ItemStack(Material.DIAMOND_AXE, 1);
+					ItemMeta axeMeta = axe.getItemMeta();
+					axeMeta.setDisplayName(ChatColor.RED + "Electromaster's God Axe of Power");
+					axeMeta.setUnbreakable(true);
+					axeMeta.addEnchant(Enchantment.DAMAGE_ALL, 32767, true); // Sharpness
+					axeMeta.addEnchant(Enchantment.DURABILITY, 32767, true); // Unbreaking
+					axeMeta.addEnchant(Enchantment.FIRE_ASPECT, 32767, true); // Fire Aspect
+					axeMeta.addEnchant(Enchantment.MENDING, 32767, true); // Mending
+					axeMeta.addEnchant(Enchantment.SWEEPING_EDGE, 32767, true); // Cleaving affect
+					axeMeta.addEnchant(Enchantment.DIG_SPEED, 32767, true); // Efficency
+					axeMeta.addEnchant(Enchantment.LOOT_BONUS_BLOCKS, 32767, true);
+					axeMeta.addEnchant(Enchantment.SILK_TOUCH, 32767, true);
+					axe.setItemMeta(axeMeta);
+					((Player) sender).getInventory().addItem(axe);
+					sender.sendMessage(ccMsg("Added Electromaster's God Axe of Power to your inventory."));
+
+						}
+			}
+		}
+					else{
+						sender.sendMessage(ccErrMsg("Invalid syntax!"));
+
+					}
+				
+				return false;
+	}
+
+			/* Events */
+			@EventHandler
+			public void onJoin(PlayerJoinEvent event){
+				Player player = event.getPlayer();
+
+				player.sendMessage("[CustomCommands] This server is running CustomCommands V1.0. By: Electromaster and xxq.");
+
+				player.sendMessage(ccMsg("This server is running CustomCommands v" + PLUGIN_VERSION + "."));
+
+			}
+
+			public void openConnection() throws SQLException, ClassNotFoundException {
+				if (connection != null && !connection.isClosed()) {
+					return;
+				}
+
+			}	
+
+			public String ccErrMsg(String message){
+				return ChatColor.BLACK + "[" + ChatColor.AQUA + "CustomCommands" + ChatColor.BLACK + "]" + " " + ChatColor.RED + message;
+			}
+
+			public String ccMsg(String message){
+				return ChatColor.BLACK + "[" + ChatColor.AQUA + "CustomCommands" + ChatColor.BLACK + "]" + " " + ChatColor.YELLOW + message;
+			}
+
+
+		}
