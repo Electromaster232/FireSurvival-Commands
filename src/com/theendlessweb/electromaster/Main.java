79e66cabba11a09750de6fd02d65f30186c7d5ae
Electromaster232 <gilbert7573@gmail.com>
2017-07-27 09:38:16 -0400

    Compiled JAR

--- src/com/theendlessweb/electromaster/Main.java
+++ src/com/theendlessweb/electromaster/Main.java
@@ -140,7 +140,7 @@ public class Main extends JavaPlugin {
 
 
 				}
-<<<<<<< HEAD
+
 			}else{
 				sender.sendMessage(ccErrMsg("Invalid syntax!"));
 				return false;
@@ -199,198 +199,204 @@ public class Main extends JavaPlugin {
 					((Player) sender).getInventory().addItem(legs);
 					((Player) sender).getInventory().addItem(boots);
 					sender.sendMessage(ccMsg("Added god armor to your inventory."));
-=======
+
+
+				} 
+				else if(command.getName().equalsIgnoreCase("tools")){
+					if(sender instanceof Player && args.length == 0){
+						if((users.get(((Player) sender).getUniqueId()) == null)){
+							ItemStack sword = new ItemStack(Material.DIAMOND_SWORD, 1);
+							ItemMeta swordMeta = sword.getItemMeta();
+							swordMeta.setDisplayName("Divine Rapier");
+							swordMeta.setUnbreakable(true);
+							swordMeta.addEnchant(Enchantment.DAMAGE_ALL, 32767, true); // Sharpness
+							swordMeta.addEnchant(Enchantment.DURABILITY, 32767, true); // Unbreaking
+							swordMeta.addEnchant(Enchantment.FIRE_ASPECT, 32767, true); // Fire Aspect
+							swordMeta.addEnchant(Enchantment.MENDING, 32767, true); // Mending
+							swordMeta.addEnchant(Enchantment.SWEEPING_EDGE, 32767, true); // Cleaving affect
+							ItemStack pick = new ItemStack(Material.DIAMOND_PICKAXE, 1);
+							ItemMeta pickMeta = pick.getItemMeta();
+							pickMeta.setDisplayName("Hoarfrost");
+							pickMeta.setUnbreakable(true);
+							pickMeta.addEnchant(Enchantment.DIG_SPEED, 5, true);
+							pickMeta.addEnchant(Enchantment.DURABILITY, 3, true); // Unbreaking
+							pickMeta.addEnchant(Enchantment.SILK_TOUCH, 1, true);
+							pickMeta.addEnchant(Enchantment.MENDING, 1, true);
+							sword.setItemMeta(swordMeta);
+							pick.setItemMeta(pickMeta);
+							((Player) sender).getInventory().addItem(sword);
+							((Player) sender).getInventory().addItem(pick);
+							sender.sendMessage(ccMsg("Added god tools to your inventory."));
+
+
+						}
+					}else{
+						sender.sendMessage(ccErrMsg("Invalid syntax!"));
+						return false;
+
+					}
+				}else{
+					sender.sendMessage(ccErrMsg("Invalid syntax!"));
+				}
+
+			}
+			else if(command.getName().equalsIgnoreCase("axe")){
+				if(sender instanceof Player && args.length == 0){
+					if((users.get(((Player) sender).getUniqueId()) == null)){
+						ItemStack axe = new ItemStack(Material.DIAMOND_AXE, 1);
+						ItemMeta axeMeta = axe.getItemMeta();
+						axeMeta.setDisplayName("§4Electromaster's God Axe of Power");
+						axeMeta.setLore(supplierNames);
+						axeMeta.setUnbreakable(true);
+						axeMeta.addEnchant(Enchantment.DAMAGE_ALL, 32767, true); // Sharpness
+						axeMeta.addEnchant(Enchantment.DURABILITY, 32767, true); // Unbreaking
+						axeMeta.addEnchant(Enchantment.FIRE_ASPECT, 32767, true); // Fire Aspect
+						axeMeta.addEnchant(Enchantment.MENDING, 32767, true); // Mending
+						axeMeta.addEnchant(Enchantment.SWEEPING_EDGE, 32767, true); // Cleaving affect
+						axeMeta.addEnchant(Enchantment.DIG_SPEED, 32767, true);
+						axe.setItemMeta(axeMeta);
+						((Player) sender).getInventory().addItem(axe);
+						sender.sendMessage(ccMsg("Added Electromaster's God Axe of Power to your inventory."));
+					}
+				}
+			}
+
+						else if(command.getName().equalsIgnoreCase("armor")){
+							if(sender instanceof Player && args.length == 0){
+								if((users.get(((Player) sender).getUniqueId()) == null)){
+									ItemStack helm = new ItemStack(Material.DIAMOND_HELMET, 1);
+									ItemStack chest = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+									ItemStack legs = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+									ItemStack boots = new ItemStack(Material.DIAMOND_BOOTS, 1);
+
+									ItemMeta helmMeta = helm.getItemMeta();
+									ItemMeta chestMeta = chest.getItemMeta();
+									ItemMeta legsMeta = legs.getItemMeta();
+									ItemMeta bootsMeta = boots.getItemMeta();
+
+									/* Names */
+									helmMeta.setDisplayName("Veil of Discord");
+									chestMeta.setDisplayName("Assault Cuirass");
+									legsMeta.setDisplayName("Shiva's Guard");
+									bootsMeta.setDisplayName("Boots of Travel");
+
+									/* Protection */
+									helmMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+									chestMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+									legsMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+									bootsMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+
+									/* Unbreaking */
+									helmMeta.addEnchant(Enchantment.DURABILITY, 3, true);
+									chestMeta.addEnchant(Enchantment.DURABILITY, 3, true);
+									legsMeta.addEnchant(Enchantment.DURABILITY, 3, true);
+									bootsMeta.addEnchant(Enchantment.DURABILITY, 3, true);
+
+									/* Mending */
+									helmMeta.addEnchant(Enchantment.MENDING, 1, true);
+									chestMeta.addEnchant(Enchantment.MENDING, 1, true);
+									legsMeta.addEnchant(Enchantment.MENDING, 1, true);
+									bootsMeta.addEnchant(Enchantment.MENDING, 1, true);
+
+									/* Misc */
+									helmMeta.addEnchant(Enchantment.OXYGEN, 3, true);
+									helmMeta.addEnchant(Enchantment.WATER_WORKER, 1, true);
+									bootsMeta.addEnchant(Enchantment.DEPTH_STRIDER, 3, true);
+									bootsMeta.addEnchant(Enchantment.PROTECTION_FALL, 4, true);
+
+									helm.setItemMeta(helmMeta);
+									chest.setItemMeta(chestMeta);
+									legs.setItemMeta(legsMeta);
+									boots.setItemMeta(bootsMeta);
+
+									((Player) sender).getInventory().addItem(helm);
+									((Player) sender).getInventory().addItem(chest);
+									((Player) sender).getInventory().addItem(legs);
+									((Player) sender).getInventory().addItem(boots);
+									sender.sendMessage(ccMsg("Added god armor to your inventory."));
+								}
+							}else{
+								sender.sendMessage(ccErrMsg("Invalid syntax!"));
+								return false;
+
+							}
+						}
+						else if(command.getName().equalsIgnoreCase("flight")){
+							if(sender instanceof Player && args.length == 0){
+								if((users.get(((Player) sender).getUniqueId()) == null)){
+									ItemStack elytra = new ItemStack(Material.ELYTRA, 1);
+									ItemMeta elytraMeta = elytra.getItemMeta();
+									elytraMeta.setDisplayName("§bStaff of Flight");
+									elytraMeta.setLore(supplierNames);
+									elytraMeta.setUnbreakable(true);
+									elytraMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 32767, true); // Sharpness
+									elytraMeta.addEnchant(Enchantment.DURABILITY, 32767, true); // Unbreaking
+									elytraMeta.addEnchant(Enchantment.THORNS, 32767, true); // Fire Aspect
+									elytraMeta.addEnchant(Enchantment.MENDING, 32767, true); // Mending
+									elytra.setItemMeta(elytraMeta);
+									((Player) sender).getInventory().addItem(elytra);
+									ItemStack firework = new ItemStack(Material.FIREWORK, 64);
+									((Player) sender).getInventory().addItem(firework);
+									sender.sendMessage(ccMsg("Added Staff of Flight to your inventory."));
+								}
+							}
+						}
+						else if(command.getName().equalsIgnoreCase("moar")){
+							Player p = (Player) sender;
+							if(sender instanceof Player && args.length == 0){
+								if(p.getInventory().getItemInMainHand().getType() != Material.AIR){
+
+									if(item.getAmount() < item.getMaxStackSize() && item.getAmount() != item.getMaxStackSize()){
+										item.setAmount(item.getMaxStackSize());
+									}		
+									if(item.getAmount() == item.getMaxStackSize()){
+										sender.sendMessage(ccErrMsg("The stack in your hand is already full."));
+									}
+
+
+									if(p.getInventory().getItemInMainHand() == null){
+										sender.sendMessage(ccErrMsg("You need to have an item in your hand."));
+										return false;
+
+									}else{
+										p.getInventory().getItemInMainHand().setAmount(p.getInventory().getItemInMainHand().getMaxStackSize());
+									}
+
+								}
+							}else{
+								sender.sendMessage(ccErrMsg("Invalid syntax!"));
+								return false;
+							}
+						}
+					}
+				
 			
-			} 
-			else if(command.getName().equalsIgnoreCase("tools")){
-				if(sender instanceof Player && args.length == 0){
-					if((users.get(((Player) sender).getUniqueId()) == null)){
-						ItemStack sword = new ItemStack(Material.DIAMOND_SWORD, 1);
-						ItemMeta swordMeta = sword.getItemMeta();
-						swordMeta.setDisplayName("Divine Rapier");
-						swordMeta.setUnbreakable(true);
-						swordMeta.addEnchant(Enchantment.DAMAGE_ALL, 32767, true); // Sharpness
-						swordMeta.addEnchant(Enchantment.DURABILITY, 32767, true); // Unbreaking
-						swordMeta.addEnchant(Enchantment.FIRE_ASPECT, 32767, true); // Fire Aspect
-						swordMeta.addEnchant(Enchantment.MENDING, 32767, true); // Mending
-						swordMeta.addEnchant(Enchantment.SWEEPING_EDGE, 32767, true); // Cleaving affect
-						ItemStack pick = new ItemStack(Material.DIAMOND_PICKAXE, 1);
-						ItemMeta pickMeta = pick.getItemMeta();
-						pickMeta.setDisplayName("Hoarfrost");
-						pickMeta.setUnbreakable(true);
-						pickMeta.addEnchant(Enchantment.DIG_SPEED, 5, true);
-						pickMeta.addEnchant(Enchantment.DURABILITY, 3, true); // Unbreaking
-						pickMeta.addEnchant(Enchantment.SILK_TOUCH, 1, true);
-						pickMeta.addEnchant(Enchantment.MENDING, 1, true);
-						sword.setItemMeta(swordMeta);
-						pick.setItemMeta(pickMeta);
-						((Player) sender).getInventory().addItem(sword);
-						((Player) sender).getInventory().addItem(pick);
-						sender.sendMessage(ccMsg("Added god tools to your inventory."));
-						
-						
-					}
-				}else{
-					sender.sendMessage(ccErrMsg("Invalid syntax!"));
-					return false;
->>>>>>> updates
-				}
-			}else{
-				sender.sendMessage(ccErrMsg("Invalid syntax!"));
-			}
-<<<<<<< HEAD
-		}
-		else if(command.getName().equalsIgnoreCase("axe")){
-			if(sender instanceof Player && args.length == 0){
-				if((users.get(((Player) sender).getUniqueId()) == null)){
-					ItemStack axe = new ItemStack(Material.DIAMOND_AXE, 1);
-					ItemMeta axeMeta = axe.getItemMeta();
-					axeMeta.setDisplayName("§4Electromaster's God Axe of Power");
-					axeMeta.setLore(supplierNames);
-					axeMeta.setUnbreakable(true);
-					axeMeta.addEnchant(Enchantment.DAMAGE_ALL, 32767, true); // Sharpness
-					axeMeta.addEnchant(Enchantment.DURABILITY, 32767, true); // Unbreaking
-					axeMeta.addEnchant(Enchantment.FIRE_ASPECT, 32767, true); // Fire Aspect
-					axeMeta.addEnchant(Enchantment.MENDING, 32767, true); // Mending
-					axeMeta.addEnchant(Enchantment.SWEEPING_EDGE, 32767, true); // Cleaving affect
-					axeMeta.addEnchant(Enchantment.DIG_SPEED, 32767, true);
-					axe.setItemMeta(axeMeta);
-					((Player) sender).getInventory().addItem(axe);
-					sender.sendMessage(ccMsg("Added Electromaster's God Axe of Power to your inventory."));
-=======
-			else if(command.getName().equalsIgnoreCase("armor")){
-				if(sender instanceof Player && args.length == 0){
-					if((users.get(((Player) sender).getUniqueId()) == null)){
-						ItemStack helm = new ItemStack(Material.DIAMOND_HELMET, 1);
-						ItemStack chest = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
-						ItemStack legs = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
-						ItemStack boots = new ItemStack(Material.DIAMOND_BOOTS, 1);
-						
-						ItemMeta helmMeta = helm.getItemMeta();
-						ItemMeta chestMeta = chest.getItemMeta();
-						ItemMeta legsMeta = legs.getItemMeta();
-						ItemMeta bootsMeta = boots.getItemMeta();
-						
-						/* Names */
-						helmMeta.setDisplayName("Veil of Discord");
-						chestMeta.setDisplayName("Assault Cuirass");
-						legsMeta.setDisplayName("Shiva's Guard");
-						bootsMeta.setDisplayName("Boots of Travel");
-						
-						/* Protection */
-						helmMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
-						chestMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
-						legsMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
-						bootsMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
-						
-						/* Unbreaking */
-						helmMeta.addEnchant(Enchantment.DURABILITY, 3, true);
-						chestMeta.addEnchant(Enchantment.DURABILITY, 3, true);
-						legsMeta.addEnchant(Enchantment.DURABILITY, 3, true);
-						bootsMeta.addEnchant(Enchantment.DURABILITY, 3, true);
-						
-						/* Mending */
-						helmMeta.addEnchant(Enchantment.MENDING, 1, true);
-						chestMeta.addEnchant(Enchantment.MENDING, 1, true);
-						legsMeta.addEnchant(Enchantment.MENDING, 1, true);
-						bootsMeta.addEnchant(Enchantment.MENDING, 1, true);
-						
-						/* Misc */
-						helmMeta.addEnchant(Enchantment.OXYGEN, 3, true);
-						helmMeta.addEnchant(Enchantment.WATER_WORKER, 1, true);
-						bootsMeta.addEnchant(Enchantment.DEPTH_STRIDER, 3, true);
-						bootsMeta.addEnchant(Enchantment.PROTECTION_FALL, 4, true);
-						
-						helm.setItemMeta(helmMeta);
-						chest.setItemMeta(chestMeta);
-						legs.setItemMeta(legsMeta);
-						boots.setItemMeta(bootsMeta);
-						
-						((Player) sender).getInventory().addItem(helm);
-						((Player) sender).getInventory().addItem(chest);
-						((Player) sender).getInventory().addItem(legs);
-						((Player) sender).getInventory().addItem(boots);
-						sender.sendMessage(ccMsg("Added god armor to your inventory."));
-					}
-				}else{
-					sender.sendMessage(ccErrMsg("Invalid syntax!"));
-					return false;
->>>>>>> updates
-				}
-			}
-			else if(command.getName().equalsIgnoreCase("flight")){
-				if(sender instanceof Player && args.length == 0){
-					if((users.get(((Player) sender).getUniqueId()) == null)){
-						ItemStack elytra = new ItemStack(Material.ELYTRA, 1);
-						ItemMeta elytraMeta = elytra.getItemMeta();
-						elytraMeta.setDisplayName("§bStaff of Flight");
-						elytraMeta.setLore(supplierNames);
-						elytraMeta.setUnbreakable(true);
-						elytraMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 32767, true); // Sharpness
-						elytraMeta.addEnchant(Enchantment.DURABILITY, 32767, true); // Unbreaking
-						elytraMeta.addEnchant(Enchantment.THORNS, 32767, true); // Fire Aspect
-						elytraMeta.addEnchant(Enchantment.MENDING, 32767, true); // Mending
-						elytra.setItemMeta(elytraMeta);
-						((Player) sender).getInventory().addItem(elytra);
-						ItemStack firework = new ItemStack(Material.FIREWORK, 64);
-						((Player) sender).getInventory().addItem(firework);
-						sender.sendMessage(ccMsg("Added Staff of Flight to your inventory."));
-					}
-				}
-			}
-			else if(command.getName().equalsIgnoreCase("moar")){
-				Player p = (Player) sender;
-				if(sender instanceof Player && args.length == 0){
-					if(p.getInventory().getItemInMainHand().getType() != Material.AIR){
-<<<<<<< HEAD
-						if(item.getAmount() < item.getMaxStackSize() && item.getAmount() != item.getMaxStackSize()){
-							item.setAmount(item.getMaxStackSize());
-						}		
-						if(item.getAmount() == item.getMaxStackSize()){
-							sender.sendMessage(ccErrMsg("The stack in your hand is already full."));
-						}
-=======
->>>>>>> updates
-						if(p.getInventory().getItemInMainHand() == null){
-							sender.sendMessage(ccErrMsg("You need to have an item in your hand."));
-							return false;
-						
-						}else{
-							p.getInventory().getItemInMainHand().setAmount(p.getInventory().getItemInMainHand().getMaxStackSize());
-						}
-
-					}
-				}else{
-					sender.sendMessage(ccErrMsg("Invalid syntax!"));
+				
 					return false;
 				}
+
+				/* Events */
+				@EventHandler
+				public void onJoin(PlayerJoinEvent event){
+					event.getPlayer().sendMessage("[CustomCommands] This server is running CustomCommands V1.0. By: Electromaster and xxq.");
+					event.getPlayer().sendMessage(ccMsg("This server is running CustomCommands v" + PLUGIN_VERSION + "."));
+				}
+
+				public void openConnection() throws SQLException, ClassNotFoundException {
+					if (connection != null && !connection.isClosed()) {
+						return;
+					}
+
+				}	
+
+				public String ccErrMsg(String message){
+					return ChatColor.BLACK + "[" + ChatColor.AQUA + "CustomCommands" + ChatColor.BLACK + "]" + " " + ChatColor.RED + message;
+				}
+
+				public String ccMsg(String message){
+					return ChatColor.BLACK + "[" + ChatColor.AQUA + "CustomCommands" + ChatColor.BLACK + "]" + " " + ChatColor.YELLOW + message;
+				}
+
+
 			}
-		}
-		return false;
-	}
-
-	/* Events */
-	@EventHandler
-	public void onJoin(PlayerJoinEvent event){
-		event.getPlayer().sendMessage("[CustomCommands] This server is running CustomCommands V1.0. By: Electromaster and xxq.");
-		event.getPlayer().sendMessage(ccMsg("This server is running CustomCommands v" + PLUGIN_VERSION + "."));
-	}
-
-	public void openConnection() throws SQLException, ClassNotFoundException {
-		if (connection != null && !connection.isClosed()) {
-			return;
-		}
-
-	}	
-
-	public String ccErrMsg(String message){
-		return ChatColor.BLACK + "[" + ChatColor.AQUA + "CustomCommands" + ChatColor.BLACK + "]" + " " + ChatColor.RED + message;
-	}
-
-	public String ccMsg(String message){
-		return ChatColor.BLACK + "[" + ChatColor.AQUA + "CustomCommands" + ChatColor.BLACK + "]" + " " + ChatColor.YELLOW + message;
-	}
-
-
-}
 
